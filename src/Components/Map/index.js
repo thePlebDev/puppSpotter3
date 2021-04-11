2442a45a2ace1bd54e21d4b100f9dc0423849e03
Tristan Elliott <>
2021-04-11 17:15:26 -0300

    adding the map

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+import './map.css';
 
 
-const Map =()=>{
+
+const MapComponent =()=>{
     return(
-        <h1>Map page</h1>
+        <MapContainer center={[46.0878, -64.7782]} zoom={7} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[46.0878, -64.7782]}>
+                <Popup>
+                    Dave the poodle spotted here
+                </Popup>
+            </Marker>
+            <Marker position={[46.0878, -65.7782]}>
+                <Popup>
+                    Tim the bulldog spotted here
+                </Popup>
+            </Marker>
+        </MapContainer>
     )
 }
 
-export default Map
+export default MapComponent
